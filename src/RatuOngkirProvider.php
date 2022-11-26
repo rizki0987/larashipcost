2d3a03d87d299919ac8c2cf7bc65848b37cfe3b0
ThiccPan <ihilmiderian@gmail.com>
2022-11-27 03:24:20 +0700

    ratuongkir update

--- src/RatuOngkirProvider.php
+++ src/RatuOngkirProvider.php
@@ -2,9 +2,11 @@
 
 namespace ThiccPan\Larashipcost;
 
+use Illuminate\Support\Facades\Http;
+
 class RatuOngkirProvider extends Provider implements ProviderBuilder
 {
-    protected $url = 'www.ratuongkir.com/';
+    protected $url = 'https://636dcc52b567eed48ac95a16.mockapi.io/ratuongkir';
 
     public function setIdProvinsi($idProvinsi): self
     {
@@ -16,6 +18,13 @@ class RatuOngkirProvider extends Provider implements ProviderBuilder
     public function setIdKota($idKota): self
     {
         $this->idKota = $idKota;
+
+        return $this;
+    }
+
+    public function setOrigin($idDestination): self
+    {
+        $this->idDestination = $idDestination;
 
         return $this;
     }
@@ -43,6 +52,93 @@ class RatuOngkirProvider extends Provider implements ProviderBuilder
 
     public function getShippingCost()
     {
-        return $this;
+        // set kota asal dan destinasi pengiriman
+        $asal = $this->getKota();
+        $asal = json_decode($asal, true);
+
+        $destinasi = $this->getDestinasi();
+        $destinasi = json_decode($destinasi, true);
+
+        // deklarasi jarak dan tarif/km untuk dimasukkan ke harga ongkir
+        $jarak = $this->countDistance(
+            $asal['latitude'],
+            $asal['longitude'],
+            $destinasi['latitude'],
+            $destinasi['longitude']
+        );
+
+        // menyesuaikan tarif dengan berat barang
+        $tarif = $this->cekBerat($this->weight, 12000);
+
+        // total ongkir = jarak pengiriman dikali tarif/km
+        $hargaOngkir = 0;
+        $hargaOngkir += ($jarak * $tarif);
+        return $hargaOngkir;
+    }
+
+    public function getAllProvinsi()
+    {
+        $response = Http::get($this->url . "/provinsi");
+        return $response->body();
+    }
+
+    public function getProvinsi()
+    {
+        $response = Http::get($this->url . "/provinsi/" . $this->idProvinsi);
+        return $response->body();
+    }
+
+    public function getAllKota()
+    {
+        $response = Http::get($this->url
+            . "/provinsi/"
+            . $this->idProvinsi
+            . "/kota");
+
+        return $response->body();
+    }
+
+    public function getKota()
+    {
+        $response = Http::get($this->url
+            . "/kota/"
+            . $this->idKota);
+
+        return $response->body();
+    }
+
+    public function getDestinasi()
+    {
+        $respones = Http::get($this->url
+            . "/kota/"
+            . $this->idDestination);
+
+        return $respones->body();
+    }
+
+    private function countDistance($latitudeAsal, $latitudeDestinasi, $longitudeAsal, $longitudeDestinasi, $earthRadius = 6371000)
+    {
+        // convert derajat ke radian
+        $latFrom = deg2rad($latitudeAsal);
+        $lonFrom = deg2rad($latitudeDestinasi);
+        $latTo = deg2rad($longitudeAsal);
+        $lonTo = deg2rad($longitudeDestinasi);
+
+        $latDelta = $latTo - $latFrom;
+        $lonDelta = $lonTo - $lonFrom;
+
+        $angle = 2 * asin(sqrt(pow(sin($latDelta / 2), 2) +
+            cos($latFrom) * cos($latTo) * pow(sin($lonDelta / 2), 2)));
+        return $angle * $earthRadius;
+    }
+
+    // cek berat pengiriman, kalau lebih dari 50 kg tarif 1.5 kali lebih mahal
+    private function cekBerat(float $berat, $tarif): float
+    {
+        if ($berat < 50) {
+            return $tarif;
+        } else {
+            return $tarif * 1.5;
+        }
     }
 }
